dee6a25f3a9984a3f003585d69a2e84028af70b3
Abdullah Al Noman <154955210+AlNomanCSE@users.noreply.github.com>
2024-09-21 15:55:34 +0600

    added EmployeeDto  (in :/dto), EmployeeMapper (in :/mapper)

---
+++ src/main/java/com/noman/ems/dto/EmployeeDto.java
@@ -0,0 +1,23 @@
+package com.noman.ems.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+//use : transfer tha data between client and server
+//will use it for response of REST API
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDto {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+}
---
+++ src/main/java/com/noman/ems/mapper/EmployeeMapper.java
@@ -0,0 +1,29 @@
+package com.noman.ems.mapper;
+
+import com.noman.ems.dto.EmployeeDto;
+import com.noman.ems.entity.Employee;
+
+//Use:
+//------
+//to map --> Employee entity ------to------ EmployeeDto
+//       --> EmployeeDto     ------to-----  Employee entity
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+    public static Employee mapToEmployeeDto(EmployeeDto employeeDto) {
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
